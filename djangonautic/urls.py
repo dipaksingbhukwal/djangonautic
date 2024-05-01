97444f4d03808977e2034282a46e14ae04e366f8
Dipaksingh Bhukwal <dipaksingbhukwal@gmail.com>
2024-05-01 07:48:59 -0700

    Adding complete project

--- djangonautic/urls.py
+++ djangonautic/urls.py
@@ -20,16 +20,16 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from articles import views as article_views
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage),
-    path('about/', views.about),
-    path('articles/', include('articles.urls')),
     path('accounts/', include('accounts.urls')),
-
+    path('articles/', include('articles.urls')),
+    path('about/', views.about),
+    path('', article_views.article_list, name="home"),    
 ]
 
 urlpatterns += staticfiles_urlpatterns()
